fdaf9629514e9cc67ff330de9e257f838667b2d2
Valentin Detcheberry <valentin.dty@gmail.com>
2018-05-16 21:36:47 +0200

    test

---
+++ src/Database/DAO_utilisateur.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace ProjectDesigner\Database;
+
+use ProjectDesigner\Models\User;
+
+class DAO_utilisateur
+{
+    private $database;
+    private $daoAddress;
+
+    public function __construct(IDatabase $database, IDAOAddress $daoAddress)
+    {
+        $this->database = $database;
+        $this->daoAddress = $daoAddress;
+    }
+
+    public function check($login, $password)
+    {
+        $results = $this->database->query(
+            "SELECT pseudo_utilisateur " .
+            "FROM Utilisateur WHERE " .
+            "pseudo_utilisateur = '" . $login . "'" .
+            " AND mdp_utilisateur = '" . $this->database->hash($password) . "';");
+
+        return $results->rowCount() > 0;
+    }
+
+    public function getConnected()
+    {
+        if (!isset($_SESSION['login']))
+            return false;
+
+        $results = $this->database->query(
+            "SELECT * FROM Utilisateur " .
+            "WHERE pseudo_utilisateur = '" . $_SESSION['login'] . "';"
+        );
+
+        if ($results->rowCount() < 1)
+            return false;
+
+        $row = $results->fetch();
+
+        $address = $this->daoAddress->get($row['id_lieu']);
+        if (!$address)
+            return false;
+
+        $user = new User(
+            $row['id_utilisateur'],
+            $row['pseudo_utilisateur'],
+            $row['mail_utilisateur'],
+            $row['prenom_utilisateur'],
+            $row['nom_utilisateur'],
+            $address
+        );
+
+        return $user;
+    }
+
+    public function get($id)
+    {
+        if (!isset($id))
+            return false;
+        if (!is_numeric($id))
+            return false;
+
+        $results = $this->database->query(
+            "SELECT * FROM Utilisateur " .
+            "WHERE id_utilisateur = " . $id . ";"
+        );
+
+        if ($results->rowCount() < 1)
+            return false;
+
+        $row = $results->fetch();
+
+        $address = $this->daoAddress->get($row['id_lieu']);
+        if (!$address)
+            return false;
+
+        $user = new User(
+            $row['id_utilisateur'],
+            $row['pseudo_utilisateur'],
+            $row['mail_utilisateur'],
+            $row['prenom_utilisateur'],
+            $row['nom_utilisateur'],
+            $address
+        );
+
+        return $user;
+    }
+
+    public function add(User $user)
+    {
+
+        $this->database->query(
+            "INSERT INTO utilisateur(" .
+            "pseudo_utilisateur, " .
+            "mail_utilisateur, " .
+            "mdp_utilisateur, " .
+            "prenom_utilisateur, " .
+            "nom_utilisateur, " .
+            "VALUES(" .
+            "'" . $user->getUsername() . "', " .
+            "'" . $user->getMail() . "', " .
+            "'" . $this->database->hash($user->getPassword()) . "', " .
+            "'" . $user->getPrenom() . "', " .
+            "'" . $user->getNom()");");
+
+        $user->setId($this->database->insertId());
+
+        return $user;
+    }
+
+    public function exists($login) {
+        $result = $this->database->query("SELECT pseudo_utilisateur FROM Utilisateur WHERE pseudo_utilisateur = '" . $login . "'");
+        return $result->rowCount() > 0;
+    }
+}
---
+++ src/Models/Utilisateur.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ;
+
+class User
+{
+    private $id;
+    private $pseudo;
+    private $mail;
+    private $nom;
+    private $prenom;
+    private $mdp;
+
+    public function __construct($id,
+                                $pseudo,
+                                $mail,
+                                $prenom,
+                                $nom,
+                                $mdp)
+    {
+        $this->id = $id;
+        $this->pseudo = $pseudo;
+        $this->mail = $mail;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->mdp = $mdp;
+    }
+}
