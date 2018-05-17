69a72af619969f3dd265b5f8c1d78fc21c8eab8e
Valentin Detcheberry <valentin.dty@gmail.com>
2018-05-17 14:48:00 +0200

    maj connexion et inscription

--- inscription.php
+++ inscription.php
@@ -15,7 +15,7 @@
         <?php include("header.php"); ?>
         <section class="mainComponet">
             <h1>Inscription</h1>
-                <form method="post" action="inscription.php">
+                <form method="post" action="traitement.php">
                     <p>
                         <label>Votre pseudo</label> : <input type="text" name="pseudo" />
                     </p>
@@ -37,8 +37,39 @@
                     <p>
                         <label>Votre pseudo</label> : <input type="text" name="pseudo" />
                     </p>
+                    <p>
+                        <input type="submit" name="M'inscrire" />
+                    </p>
                 </form>
         </section>
         <?php include 'footer.php'; ?>
     </body>
 </html>
+
+<?php
+
+if(!empty($_POST['pseudo']))
+{
+    //connection BD
+    mysql_connect("localhost", "root", "");
+    mysql_select_db("projectdesigner");
+
+    $mdp1 = mysql_real_escape_string(htmlspecialchars($_POST['mdp1']));
+    $mdp2 = mysql_real_escape_string(htmlspecialchars($_POST['mdp2']));
+    if($mdp1 == $mdp2) // vérification mdp
+    {
+        $pseudo = mysql_real_escape_string(htmlspecialchars($_POST['pseudo']));
+        $mail = mysql_real_escape_string(htmlspecialchars($_POST['mail']));
+        // cryptage mdp :
+        //$mdp1 = sha1($mdp1);
+
+        mysql_query("INSERT INTO Utilisateur VALUES('', '$nom', '$prenom', '$pseudo', '$mdp1', '$mail')");
+    }
+ 
+    else
+    {
+        echo 'Les deux mots de passe que vous avez rentrés ne correspondent pas.';
+    }
+}
+
+?>
