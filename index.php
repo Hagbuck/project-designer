30131790ea3345778616805b5ec42512e197832e
Drainman <cor.troa@gmail.com>
2018-05-18 09:05:30 +0200

    Version Alpha prez

--- index.php
+++ index.php
@@ -18,16 +18,13 @@
             <h1>Project Designer</h1>
 
             <p>
-                <span clas="projectName">Project Designer</span> est un site web ayant pour but de faciliter l'élaboration de vos projets. Nous mettons donc à votre édition un outils de pré-conception.
-                <br> Evhozieh oizhg operjboipeth pozek fpoekjpz kfpiboe^rkg zonbôrze jbêonb
+                <span clas="projectName">Project Designer</span> est un site web ayant pour but de faciliter l'élaboration de vos projets. Nous mettons à votre disponibilités des outils de pré-conception.
                 <br /><br />
-                Créez, éditez et repartissez vos idée par grande thématique afin d'obtenir une vision global de votre projet.
+                Créez, éditez et repartissez vos idées par grande thématique afin d'obtenir une vision global de votre projet.
                 <br /><br />
-                Mauris vel luctus enim. Proin nec dictum mi. Nulla eleifend finibus diam in feugiat. Vivamus rutrum tortor luctus leo ornare rutrum. Donec interdum nibh et ipsum fringilla tempus. Donec eget ipsum vel enim scelerisque feugiat a vitae lorem. Nullam iaculis blandit accumsan. Ut posuere ultrices elit eu volutpat. Nunc quis sapien et tellus euismod bibendum in vel urna. Aliquam nec erat ut diam fringilla ultrices. Vivamus a diam venenatis, euismod ligula fermentum, interdum turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce at libero urna.
+                Partagez vos diagrammes avec vos collaborateurs et choisissez en groupe quels seront les points les plus importants de votre futur projet.
                 <br /><br />
-                Aenean blandit semper erat sit amet dictum. Aliquam ac consequat erat, vitae sagittis augue. Nulla efficitur id felis non eleifend. Donec dignissim dui a ligula accumsan efficitur. Duis eu convallis lectus, nec aliquet felis. Suspendisse quis ipsum posuere, scelerisque nisi non, gravida risus. Vivamus a elit sit amet felis vehicula gravida. Duis egestas sit amet felis quis viverra. Curabitur ullamcorper molestie egestas. Ut condimentum blandit tellus et ullamcorper. Donec sed mi in mi fermentum congue. Integer at tempor ex.
-                <br /><br />
-                Ut imperdiet tellus non nisi pretium ultrices. Suspendisse facilisis vestibulum maximus. Proin ante ante, efficitur sit amet nunc id, sollicitudin porttitor tellus. Donec dignissim pharetra dui et efficitur. Curabitur sit amet interdum sapien. Donec vestibulum mauris dui, ac iaculis sapien dignissim id. Suspendisse a gravida purus, non lacinia dolor. Aliquam blandit odio a neque pulvinar consequat. Suspendisse sit amet diam imperdiet, tincidunt ante ac, finibus mi. Vestibulum condimentum suscipit velit porta blandit. Donec pretium eu felis non sagittis. Aliquam rutrum felis ex, id dictum tellus aliquet at.
+                <b>Transformez vos idées en projet !</b>
             </p>
         </section>
         <?php include 'footer.php'; ?>
